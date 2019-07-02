39a65580c7bc4cba9b6c027742c3feb099a67dc7
EliaDamen <eliadmn@gmail.com>
2019-07-02 10:56:32 +0200

    classe record

---
+++ src/main/java/com/example/demo/Record.java
@@ -0,0 +1,63 @@
+	package com.example.demo;
+/**
+ * classe che modella i record del dataset
+ * @author Elia
+ *
+ */
+
+
+public class Record{
+
+	//oggetti freq, geo, unit, aid_instr/time_period 2000-2017
+	//metodi per elenco attributi e tipo, dati json, statistiche sui dati(uso override)
+		
+		
+		private String freq;
+		private String geo;
+		private String unit;
+		private String instrtimeperiod;
+		private int[] year  = new int[18]; 
+		
+		/**
+		 * 
+		 * @param freqin
+		 * @param geoin
+		 * @param unitin
+		 * @param instrtimeperiodin
+		 * @param yearin  è un array statico, dato che la dimensione in input sarà sampre costante
+		 */
+		public Record(String freqin, String geoin, String unitin, String instrtimeperiodin, int[] yearin) {
+			
+		this.freq=freqin;
+		this.geo=geoin;
+		this.unit=unitin;
+		this.instrtimeperiod=instrtimeperiodin;
+		System.arraycopy(yearin, 0, this.year, 0, 18);
+			
+		}
+		/**
+		 * definisco i metodi get per accedere alle variabili private
+		 * 
+		 */
+		public String getFreq() {
+			return this.freq;
+		}
+	    
+		public String getGeo() {
+			return this.geo;
+		}
+		
+		public String getUnit() {
+			return this.unit;
+		}
+		
+		public String getInstrtimeperiod() {   
+			return this.instrtimeperiod;
+		}
+		public int[] getYear() {
+			return this.year;
+		}
+	}
+
+	
+
