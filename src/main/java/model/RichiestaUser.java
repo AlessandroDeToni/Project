b88fe9d874c3ca047b32bafa996bc3bb36540949
EliaDamen <eliadmn@gmail.com>
2019-07-11 12:41:36 +0200

    SuperClasse per le richieste dell'utente

---
+++ src/main/java/model/RichiestaUser.java
@@ -0,0 +1,39 @@
+package model;
+import java.awt.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+
+public class RichiestaUser {
+
+	public Collection<Object> getCampo(Lista in, String fieldName) {
+		Collection<Object> out = new ArrayList<Object>();
+		for(Object record :in.getList()) {
+			try{
+				
+		       Method m = record.getClass().getMethod("get"+fieldName.substring(0, 1).toUpperCase()+fieldName.substring(1),null);
+		       try {
+		          Object campo= m.invoke(record);
+		          out.add(campo); 
+		       }
+		       catch(IllegalAccessException e) {
+				e.printStackTrace();
+			   }
+			   catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			   } catch (InvocationTargetException e) {
+				e.printStackTrace();
+			   }
+			}
+			catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			}	
+		}
+		return out;
+	} 		
+}
